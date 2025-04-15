cb035d8454913e8e17c14cf4cbad9847a011d147
hmztplinv <hmz.tpal@gmail.com>
2025-04-15 19:03:29 +0300

    first commit

---
+++ NoteApp.Infrastructure/DependencyInjection/InfrastructureServiceRegistration.cs
@@ -0,0 +1,11 @@
+using Microsoft.Extensions.DependencyInjection;
+
+
+public static class InfrastructureServiceRegistration
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
+    {
+
+        return services;
+    }
+}
